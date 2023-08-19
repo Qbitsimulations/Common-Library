8a4a8d44386b9bafc2b91f9f66f3516263595fe7
Yago Martínez <yandmtech2@gmail.com>
2023-08-19 12:12:31 +0200

    more AS 131-9 code and simulation

--- qbt-common/src/wasm/systems/systems/src/apu/allied_signal_131-9.rs
+++ qbt-common/src/wasm/systems/systems/src/apu/allied_signal_131-9.rs
@@ -6,20 +6,108 @@ use uom::si::{
     time::second,
 };
 
-use super::{Turbine, TurbineState};
+use super::{Turbine, TurbineState, TurbineSignal, ApuState};
+//==============================================================================
+// Allied Signal 131-9
+//==============================================================================
+
+//==============================================================================
+// States Declaration
+//==============================================================================
+
+struct Stop{
+    egt: ThermodynamicTemperature,
+    OAT: ThermodynamicTemperature,
+    ApuState: ApuState,
+}
+
+impl Stop{
+    fn pub fn new() -> Self {
+        Stop {
+            OAT : ThermodynamicTemperature::new::<degree_celsius>(0.),
+            egt: ThermodynamicTemperature::new::<degree_celsius>(OAT),
+        }
+    }
+}
+
+impl Turbine for Stop{
+    fn update( mut self: Box<Self>, context: &UpdateContext, state: ApuState) -> Box<dyn Turbine> {
+        self.egt = self.OAT;
+        match state {
+            Off => self,
+            Start => Box::new(Starting::new(self.egt, self.OAT)),
+        }
+    }
+}
+
 struct Starting{
     since: Duration,
     egt: ThermodynamicTemperature,
     OAT: ThermodynamicTemperature,
+
 }
-impl Starting {
-    fn new(egt: ThermodynamicTemperature) -> Starting{
+
+impl Starting{
+    fn pub fn new(egt: ThermodynamicTemperature, OAT: ThermodynamicTemperature) -> Self {
         Starting {
             since: Duration::from_secs(0),
-            egt,
+            OAT : ThermodynamicTemperature::new::<degree_celsius>(0.),
+            egt: ThermodynamicTemperature::new::<degree_celsius>(OAT),
         }
     }
 
-    fn  
+    fn calculate_egt(&mut self, context: &UpdateContext) -> ThermodynamicTemperature {
+        //Bare aproximation
+        self.egt = egt;
+        let egt = self.OAT
+        if (self.since.as_secs() <= 10 && egt =! 100) {
+            egt = self.OAT + (self.since.as_secs() as f64 * 0.5);
+        } else if (egt =! 60) {
+            egt = egt - 0.5;
+        } else {
+            egt = 60;
+        }
+        return egt;
+    }
 }
-impl Turbine 
+
+impl Turbine for Starting {
+    fn update(mut self: Box<Self>, context: &UpdateContext, state: ApuState) -> Box<dyn Turbine> {
+        self.since += context.delta();
+        self.egt = self.calculate_egt(context);
+        match state {
+            Stop => Box::new(Stop::new(self.egt, self.OAT)),
+            Start | Running => 
+                if {self.egt.get<ThermodynamicTemperature>() >= 100 } => {
+                    Box::new(Running::new(self.egt, self.OAT))
+                } else => self,
+        }
+    }
+    fn egt(&self) -> ThermodynamicTemperature {
+        return self.egt;
+    }
+    fn state(&self) -> TurbineState {
+        return TurbineState::Starting;
+    }
+}
+struct Running{
+    egt: ThermodynamicTemperature,
+    OAT: ThermodynamicTemperature,
+    N1: Ratio,
+    N2: Ratio,
+    FF: MassFlowRate,
+    ITT: ThermodynamicTemperature,
+    OilP: Pressure,
+    OilT: ThermodynamicTe
+}
+//==============================================================================
+// Had to stop here as an electrical grid should be implemented first
+//==============================================================================
+impl Running for Running {
+
+}
+struct ShutDown{
+    since: Duration,
+    egt: ThermodynamicTemperature,
+    OAT: ThermodynamicTemperature,
+}
---
+++ qbt-common/src/wasm/systems/systems/src/simulation/simulation.md
@@ -0,0 +1,10 @@
+# Simulation Folder
+![FlyByWire Simulations](https://raw.githubusercontent.com/flybywiresim/branding/1391fc003d8b5d439d01ad86e2778ae0bfc8b682/tails-with-text/FBW-Color-Light.svg#gh-dark-mode-only)
+This folder in its entirety was developed by the FlyByWire Team, all rights are reserved to them. 
+## License
+
+Original source code assets present in this repository are licensed under the GNU GPLv3.
+
+Microsoft Flight Simulator © Microsoft Corporation. The FlyByWire Simulations A32NX was created under Microsoft's "Game Content Usage Rules" using assets from Microsoft Flight Simulator, and it is not endorsed by or affiliated with Microsoft.
+
+- in the case of original source code from FBW or compiled artifacts generated from it, under GPLv3.
